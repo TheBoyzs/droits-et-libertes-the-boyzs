23f4d2da605725ccb719daf2a535501aaa85c509
maikhamel <hamm3157@gmail.com>
2024-10-01 17:38:05 -0400

    ajustements

---
+++ main.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector('.hamburger');
+        const mobileMenuOverlay = document.querySelector('.mobile-menu-overlay');
+
+        hamburger.addEventListener('click', () => {
+            mobileMenuOverlay.classList.toggle('show');
+            hamburger.classList.toggle('open');
+        });
+        const closeButton = document.getElementById("disclaimer-close");
+        const disclaimer = document.getElementById("disclaimer");
+        closeButton.addEventListener('click', () => {
+            disclaimer.style.display = "none";
+        });
