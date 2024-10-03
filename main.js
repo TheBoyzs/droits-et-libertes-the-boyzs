d531074694d2e53f07a00e3fcf9334a3046095d4
maikhamel <hamm3157@gmail.com>
2024-10-03 09:28:01 -0400

    début carousel swiper

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
-const hamburger = document.querySelector('.hamburger');
-        const mobileMenuOverlay = document.querySelector('.mobile-menu-overlay');
 
-        hamburger.addEventListener('click', () => {
-            mobileMenuOverlay.classList.toggle('show');
-            hamburger.classList.toggle('open');
-        });
-        const closeButton = document.getElementById("disclaimer-close");
-        const disclaimer = document.getElementById("disclaimer");
-        closeButton.addEventListener('click', () => {
-            disclaimer.style.display = "none";
-        });
+
+
+
+const hamburger = document.querySelector('.hamburger');
+const mobileMenuOverlay = document.querySelector('.mobile-menu-overlay');
+
+hamburger.addEventListener('click', () => {
+    mobileMenuOverlay.classList.toggle('show');
+    hamburger.classList.toggle('open');
+});
+const closeButton = document.getElementById("disclaimer-close");
+const disclaimer = document.getElementById("disclaimer");
+closeButton.addEventListener('click', () => {
+    disclaimer.style.display = "none";
+});
+
+/*
+let swiper = new Swiper(".swiper", {
+    loop:true,
+    slidesPerView: 1,
+    //spaceBetween: 100,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+     delay: 500,
+    },
+   
+});
+*/
