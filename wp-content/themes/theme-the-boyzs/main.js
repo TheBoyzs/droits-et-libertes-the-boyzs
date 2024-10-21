77fe7971c61d54847a8bdfb469b708a11a8891da
KentCShit <94623626+RaphBarniques@users.noreply.github.com>
2024-10-21 12:16:27 -0400

    updated scripts link path

--- wp-content/themes/theme-the-boyzs/main.js
+++ wp-content/themes/theme-the-boyzs/main.js
@@ -17,3 +17,19 @@ closeButton.addEventListener("click", () => {
 $("li").click(function () {
   $(this).addClass("active").siblings().removeClass("active");
 });
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  slidesPerView: 1,
+  //spaceBetween: 100,
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+      el: ".swiper-pagination",
+    },
+  autoplay: {
+   delay: 3000,
+  },
+});
